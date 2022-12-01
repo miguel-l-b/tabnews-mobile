a3c3f52b1c2e0ac7c8f8350c2083749632b8372d
miguel-l-b <miguelcoruj@gmail.com>
2022-12-01 00:08:04 -0300

    starting style `View`

---
+++ styles/View.styles.ts
@@ -0,0 +1,65 @@
+import { StyleSheet } from "react-native"
+
+export default StyleSheet.create({
+    padding_x1: {
+        padding: 10
+    },
+    padding_x2: {
+        padding: 20
+    },
+    padding_x3: {
+        padding: 30
+    },
+    padding_x4: {
+        padding: 40
+    },
+    padding_x5: {
+        padding: 50
+    },
+    padding_x6: {
+        padding: 60
+    },
+    padding_x7: {
+        padding: 70
+    },
+    padding_x8: {
+        padding: 80
+    },
+    padding_x9: {
+        padding: 90
+    },
+    padding_x10: {
+        padding: 100
+    },
+
+    margin_x1: {
+        margin: 10
+    },
+    margin_x2: {
+        margin: 20
+    },
+    margin_x3: {
+        margin: 30
+    },
+    margin_x4: {
+        margin: 40
+    },
+    margin_x5: {
+        margin: 50
+    },
+    margin_x6: {
+        margin: 60
+    },
+    margin_x7: {
+        margin: 70
+    },
+    margin_x8: {
+        margin: 80
+    },
+    margin_x9: {
+        margin: 90
+    },
+    margin_x10: {
+        margin: 100
+    },
+})
